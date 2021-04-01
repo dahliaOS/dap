cee26a551060a91f13e9ad91c2576245f3537912
Noah Cain <nmcain@gmail.com>
2021-03-31 19:43:17 -0700

    modify integrity agent

--- tools/integrity_agent/lib/integrity_agent.dart
+++ tools/integrity_agent/lib/integrity_agent.dart
@@ -4,23 +4,25 @@ import 'dart:convert';
 import 'manifest_model.dart';
 import "package:path/path.dart" show dirname;
 
-
+var officialRepository = 'https://nmcain.github.io/packages';
 
 void CheckApplicationManifest() async {
-  String appDir =  dirname(Platform.script.toString()).substring(7);
-  ProcessResult jsonCheck = await Process.run('cat', [appDir+'/manifest.json']);
-  String rawJson = jsonCheck.stdout.toString();
+  //var appDir = dirname(Platform.script.toString()).substring(7);
+  print('Reading' + officialRepository + '/manifest.json');
+  var jsonCheck =
+      await Process.run('curl', [officialRepository + '/manifest.json']);
+  var rawJson = jsonCheck.stdout.toString();
   print(rawJson);
   final jsonString = rawJson;
   final json = jsonDecode(jsonString);
   final manifest = Manifest.fromJson(json);
-  
 }
 
-void CheckHash(){
-  String appDir =  Platform.script.toString().substring(7);
+void CheckHash() {
+  var appDir = Platform.script.toString().substring(7);
   String executable;
-  Process.run('find', ['.','-maxdepth','1','-perm','-111','-type','-f' ]).then((ProcessResult results) {
+  Process.run('find', ['.', '-maxdepth', '1', '-perm', '-111', '-type', '-f'])
+      .then((ProcessResult results) {
     executable = results.stdout.toString().substring(2);
   });
   print(executable);
