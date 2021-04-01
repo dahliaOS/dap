a42fa6346617fd55acba275fbf4516816888f55e
Noah Cain <nmcain@gmail.com>
2021-03-31 19:43:06 -0700

    Work out logic

--- bin/pkg.dart
+++ bin/pkg.dart
@@ -3,30 +3,26 @@ import 'package:args/args.dart';
 import 'dart:io';
 
 void main(List<String> arguments) {
-  const pkgManagerName = 'pkg';
+  const pkgManagerName = 'dap';
   if (arguments[0] == 'install') {
     if (arguments.contains('-y') && arguments.length >= 2) {
       // Install the package with no input from the user
-pkg.validateInfo(arguments);
-      //pkg.installApplication(name, false);
-      
-    }
-    else if (arguments.contains("-y") == false && arguments.length >= 2){
+      pkg.validateInfo(arguments);
+      //print(pkg.architecture);
+    } else if (arguments.contains("-y") == false && arguments.length >= 2) {
       // Ask the user to confirm before installing the package
       print('did not contain y, good');
-    }
-    else {
+    } else {
       // Indicate that no package was specified in input
-      print(pkgManagerName.toUpperCase()+': ERROR: no package specified');
+      print(pkgManagerName.toUpperCase() + ': ERROR: no package specified');
     }
-  }
-  else if (arguments[0] == 'search'){
+  } else if (arguments[0] == 'search') {
     // Query for package from database using command input, and print results
     print("yep, that's a search");
-  } 
-
-  else {
+  } else {
     // Print an error to indicate the command is not valid
-    print(pkgManagerName.toUpperCase()+': ERROR: unknown command ' + arguments[0]);
+    print(pkgManagerName.toUpperCase() +
+        ': ERROR: unknown command ' +
+        arguments[0]);
   }
 }
