783a4cca4c8c700d1c103ad1b3a86119850f7c77
Noah Cain <nmcain@gmail.com>
2021-04-01 21:41:17 -0700

    Get architecture of system; determine if package architecture is correct

--- lib/pkg.dart
+++ lib/pkg.dart
@@ -4,18 +4,24 @@ import 'packageModel.dart';
 import 'dart:async';
 import 'dart:convert';
 
+const pkgManagerName = 'dap';
 var officialRepository = 'https://nmcain.github.io/packages/';
 var applicationPath = Platform.environment['HOME'] + '/Applications';
-//Init packatge values
+//Check if mutiple packages are selected for install
+List applications;
+
+//Init package values
 var dapFile;
 var realName;
 var version;
 var description;
 var hash;
 var architecture;
+var altName;
+bool installable;
 
 void validateInfo(List input) {
-  var applications = List<String>.from(input);
+  applications = List<String>.from(input);
   applications.removeWhere((item) => item == '-y');
   applications.removeWhere((item) => item == 'install');
   bool isPlural = applications.length > 1;
@@ -26,13 +32,17 @@ void validateInfo(List input) {
   CheckApplicationManifest(applications[0].toString());
 }
 
-void CheckApplicationManifest(String inputAltName) async {
+void CheckApplicationManifest(String inputAltName) {
   //print(inputAltName);
   //var appDir = dirname(Platform.script.toString()).substring(7);
   print('Reading ' + officialRepository + 'manifest.json');
-  var jsonCheck =
-      await Process.run('curl', [officialRepository + 'manifest.json']);
-  var rawJson = jsonCheck.stdout.toString();
+
+  var sysArch =
+      Process.runSync('uname', ['-m']).stdout.toString().replaceAll('\n', '');
+
+  var rawJson = Process.runSync('curl', [officialRepository + 'manifest.json'])
+      .stdout
+      .toString();
 
   final jsonString = rawJson;
   final json = jsonDecode(jsonString);
@@ -56,9 +66,41 @@ void CheckApplicationManifest(String inputAltName) async {
   architecture = manifest.packages
       .firstWhere((e) => e.altName == inputAltName, orElse: () => null)
       .architecture;
-  print(realName);
-  //await downloadFile(applicationPath, officialRepository, dapFile);
-  print('Complete');
+  altName = manifest.packages
+      .firstWhere((e) => e.altName == inputAltName, orElse: () => null)
+      .altName;
+  // ignore: prefer_single_quotes
+  if (architecture != sysArch && architecture != "none") {
+    print('The package "' +
+        dapFile +
+        '" (' +
+        architecture +
+        ') is not compatible with your device (' +
+        sysArch +
+        ')');
+    installable = false;
+  } else {
+    installable = true;
+  }
+}
+
+void validateInstall() {
+  if (!installable) {
+    print(pkgManagerName.toUpperCase() + ': ERROR: incompatible architecture!');
+    exit(1);
+  }
+  print("Selected package: " +
+      realName +
+      " - version " +
+      version +
+      " - " +
+      description);
+  stdout.write("Do you want to continue? [Y/n] ");
+  String consent = stdin.readLineSync();
+  if (consent != "Y" && consent != "y") {
+    print(pkgManagerName.toUpperCase() + ': Operation cancelled');
+    exit(0);
+  }
 }
 
 bool downloadFile(String installationPath, String repository, String dapFile) {
