679eb4b90b5856a048e566f77e765a95686fca5e
Noah Cain <nmcain@gmail.com>
2021-04-02 15:53:45 -0700

    Add search command

--- bin/pkg.dart
+++ bin/pkg.dart
@@ -6,6 +6,7 @@ void main(List<String> arguments) {
   if (arguments[0] == 'install') {
     if (arguments.contains('-y') && arguments.length >= 2) {
       // Install the package with no input from the user
+      print(arguments.length.toInt());
       pkg.validateInfo(arguments);
       //TODO, in the future, use this to iiterate beween the list pkg.applications.forEach((element) => );
       print('Selected package: ' +
@@ -47,9 +48,11 @@ void main(List<String> arguments) {
       // Indicate that no package was specified in input
       print(pkg.pkgManagerName.toUpperCase() + ': ERROR: no package specified');
     }
+  } else if (arguments[0] == 'remove') {
   } else if (arguments[0] == 'search') {
-    // Query for package from database using command input, and print results
-    print("yep, that's a search");
+    pkg.searchInfo(arguments);
+  } else if (arguments.length <= 1) {
+    print(pkg.pkgManagerName.toUpperCase() + ': ERROR: unknown command ');
   } else {
     // Print an error to indicate the command is not valid
     print(pkg.pkgManagerName.toUpperCase() +
