f9be3fb6663fd87542bdeaf5dd5a21d55101aa47
Noah Cain <nmcain@gmail.com>
2021-04-02 15:18:24 -0700

    Validate install without -y flag

--- bin/pkg.dart
+++ bin/pkg.dart
@@ -26,7 +26,23 @@ void main(List<String> arguments) {
       //print(pkg.architecture);
     } else if (arguments.contains("-y") == false && arguments.length >= 2) {
       // Ask the user to confirm before installing the package
-      print('did not contain y, good');
+      pkg.validateInfo(arguments);
+      //TODO, in the future, use this to iiterate beween the list pkg.applications.forEach((element) => );
+      print('Selected package: ' +
+          pkg.realName +
+          ' - version ' +
+          pkg.version +
+          ' - ' +
+          pkg.description);
+      pkg.validateInstall();
+      pkg.checkArchitecture();
+      pkg.downloadFile(
+          pkg.applicationPath, pkg.officialRepository, pkg.dapFile);
+      pkg.installApplication();
+      print(pkg.realName +
+          ' has been successfully installed. Launch it from the command line with: "' +
+          pkg.altName +
+          '"');
     } else {
       // Indicate that no package was specified in input
       print(pkg.pkgManagerName.toUpperCase() + ': ERROR: no package specified');
